ea20e9ea3b3a4c183936efbf72008f360af64118
lloyd aleister bonds <lloydabonds@gmail.com>
2025-12-09 20:59:12 -0500

    sixth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/personal-website"><App /></BrowserRouter>
+    <BrowserRouter basename="/personal-website/"><App /></BrowserRouter>
   </StrictMode>
 )
