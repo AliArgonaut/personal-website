d39ec31eb65ce603093e959c6036b8181fb0aa13
lloyd aleister bonds <lloydabonds@gmail.com>
2025-12-11 02:47:10 -0500

    removed unused social link from link bar

--- frontend/src/components/LinkBar.jsx
+++ frontend/src/components/LinkBar.jsx
@@ -1,8 +1,7 @@
-import { Github, Twitter, Linkedin } from 'lucide-react'
+import { Github, Linkedin } from 'lucide-react'
 export default function LinkBar () {
   return (
     <div className=" max-w-1/2 flex flex-row text-black gap-10">
-      <a href="https://x.com/omarchyuser"><Twitter /></a>
       <a href="https://github.com/AliArgonaut">  <Github /></a>
        <a href="www.linkedin.com/in/lloyd-bonds-460978214"> <Linkedin /></a>
     </div>
